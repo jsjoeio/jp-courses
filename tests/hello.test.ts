14ec9e735d1641a779268e74e6ddfd0343bc65fb
Joe Previte <jjprevite@gmail.com>
2021-04-16 08:28:33 -0700

    refactor: delete hello.test.ts

--- tests/hello.test.ts
+++
@@ -1,7 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts"
-
-Deno.test("hello world", () => {
-  const actual = () => "Hello world!"
-  const expected = "Hello world!"
-  assertEquals(actual, expected)
-})
