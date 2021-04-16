a7d1ef0484b9eb20bd10c74dcb44cca3f4652c0a
Joe Previte <jjprevite@gmail.com>
2021-04-16 08:29:00 -0700

    feat: add --help flag to main

--- main.ts
+++ main.ts
@@ -1,1 +1,54 @@
-console.log("hello!")
+export const ERROR_MESSAGE_TEMPLATE = `❌ ERROR:`;
+export const UNSUPPORTED_ARG = (arg: string) =>
+  `Received unsupported arg or flag ${arg}. Please run --help to see all flags.`;
+export const HELP_MESSAGE = `
+Downloads the $COURSE_NAME for paid users.
+
+USAGE:
+  $install_method [OPTIONS] (-i|--paymentId) <paymentId>
+
+OPTIONS:
+  -d, --dry-run
+      Prints the commands for the download process without running them.
+
+  -h, --help
+      Prints help information
+
+ARGS:
+  -i, --paymentId
+      Required. Verifies course purchase.
+      Example: $install_method --paymentId cs_live_a1VHFUz7lYnXOL3PUus13VbktedDQDubwfew8E70EvnS1BTOfNTSUXqO0i
+
+More information can be found at https://github.com/jsjoeio/jp-courses-install
+`;
+
+/**
+ * The main script that's called
+ */
+export function main(args: string[]): void {
+  args.forEach((arg) => {
+    switch (arg) {
+      case "--help":
+        console.log(HELP_MESSAGE);
+        break;
+
+      default: {
+        const unsupportedArgMessage = UNSUPPORTED_ARG(arg);
+        throwErrorMessage(
+          unsupportedArgMessage,
+        );
+        break;
+      }
+    }
+  });
+}
+
+/**
+ * Throws an error message using the ERROR_MESSAGE_TEMPLATE
+ * and the message passed in
+ */
+export function throwErrorMessage(msg: string): Error {
+  throw new Error(`${ERROR_MESSAGE_TEMPLATE} ${msg}`);
+}
+
+main(Deno.args);
