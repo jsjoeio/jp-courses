947529f9c0b5d7e6433a161c9bcd82a12ee60205
Joe Previte <jjprevite@gmail.com>
2021-04-28 07:48:41 -0700

    feat: add install script

---
+++ scripts/install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# shellcheck shell=dash
+
+main() {
+  echo "⬇️ Downloading jp-courses tool"
+
+  # Download latest release from GitHub
+  # TODO write function to get latest release then update URL
+  curl https://github.com/jsjoeio/jp-courses-install/releases/download/0.0.3/jp-courses-v0.0.3-x86_64-apple-darwin.zip -L -o jp-courses.zip
+
+  # Unzip it
+  unzip jp-courses.zip
+
+  # Make it executable
+  chmod +x dist/jp-courses-v0.0.3-x86_64-apple-darwin/jp-courses
+
+  # Remove zip
+  rm jp-courses.zip
+
+  echo "✅ Complete"
+}
+
+main "$@"
