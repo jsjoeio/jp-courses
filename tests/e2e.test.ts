02de09f176d645d84842c60b65eb506c480de87e
Joe Previte <jjprevite@gmail.com>
2021-04-28 08:47:06 -0700

    wip: add notes to e2e test

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,5 +1,31 @@
-/*
-  TODO
-  These will be tests that actually build
-  and run the script in a test environment
-*/
+// End-to-End Tests
+// Any tests that use the actual build
+// i.e. denon build
+import { exists } from "https://deno.land/std@0.93.0/fs/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts";
+
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  test,
+} from "https://x.nest.land/hooked-describe@0.1.0/mod.ts";
+
+describe("jp courses", () => {
+  beforeEach(() => {
+    // Delete everything in /dist
+    // Run denon build
+  })
+
+  afterEach(() => {
+    // Delete everything in /dist
+  })
+
+  test("should build before each test runs", async () => {
+    // TODO get local OS
+    // TODO check if zip exists for that one
+    const zipExists = await exists("./dist/")
+    // TODO later, well refactor berfore to actually unzip
+    // probably to tmp dir
+  })
+})
