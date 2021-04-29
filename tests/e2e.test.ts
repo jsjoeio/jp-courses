643a4ed5646d39018f0cd5563d53293868982b46
Joe Previte <jjprevite@gmail.com>
2021-04-29 06:09:43 -0700

    feat: add basic e2e test

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -12,20 +12,35 @@ import {
 } from "https://x.nest.land/hooked-describe@0.1.0/mod.ts";
 
 describe("jp courses", () => {
-  beforeEach(() => {
-    // Delete everything in /dist
-    // Run denon build
-  })
+  beforeEach(async () => {
+    // Create a subprocess to build projects
+    const p = await Deno.run({ cmd: ["denon", "build"] });
 
-  afterEach(() => {
+    // Wait till it's completed
+    await p.status();
+
+    p.close();
+  });
+
+  afterEach(async () => {
     // Delete everything in /dist
-  })
+    // Create a subprocess to run clean
+    const p = await Deno.run({ cmd: ["denon", "clean"] });
+
+    // Wait till it's completed
+    await p.status();
+
+    p.close();
+  });
 
   test("should build before each test runs", async () => {
     // TODO get local OS
     // TODO check if zip exists for that one
-    const zipExists = await exists("./dist/")
+    const VERSION = "0.0.3";
+    const zipName = `jp-courses-v${VERSION}-x86_64-apple-darwin.zip`;
+    const zipExists = await exists(`./dist/${zipName}`);
+    assertEquals(zipExists, true);
     // TODO later, well refactor berfore to actually unzip
     // probably to tmp dir
-  })
-})
+  });
+});
